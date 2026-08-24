e55d50769e0a16a13c8eb64279e3c0f382cd0dc3
techy-robot <asher.j.edwards1@gmail.com>
2026-08-23 23:16:34 -0600

    Switch to endstop-like 2-stage Z probing

--- klippy/extras/three_axis_probe.py
+++ klippy/extras/three_axis_probe.py
@@ -214,15 +214,22 @@ class ThreeAxisProbe:
             max_z = 999999.0
 
         lift_z = min(max(cur_pos[2], self.z_hop, (self.z_expected - existing_offset_z) + self.z_hop), max_z)
-        approach_z = min(max((self.z_expected - existing_offset_z) + self.z_hop, self.z_expected - existing_offset_z + 2.0), max_z)
-
-        # 1. Approach probe location (high-speed XY travel at lift_z, then fast Z descent to approach_z)
+        
+        # 1. Move XY over probe at safe high altitude
         toolhead.manual_move([cur_pos[0], cur_pos[1], lift_z], 50.0)
         toolhead.manual_move([target_x, target_y, lift_z], 150.0)
-        toolhead.manual_move([target_x, target_y, approach_z], 50.0)
         
-        # 2. Probe Z-axis top apex (slow probing move from approach_z down past z_expected by dist)
+        # 2. Probe Z-axis top apex
         target_z_carriage = (self.z_expected - existing_offset_z) - dist
+        fast_speed = max(speed * 6.0, 15.0)
+
+        # Stage 1: Fast search probe down from lift_z (stops immediately on contact)
+        if lift_z > (self.z_expected - existing_offset_z) + self.z_hop + 2.0:
+            pos_z_fast = self._probing_move(toolhead, [target_x, target_y, target_z_carriage], fast_speed)
+            safe_z = min(pos_z_fast[2] + self.z_hop, max_z)
+            toolhead.manual_move([target_x, target_y, safe_z], 50.0)
+
+        # Stage 2: Precision Z probe move at configured slow speed
         pos_z = self._probing_move(toolhead, [target_x, target_y, target_z_carriage], speed)
         z_apex = pos_z[2] + existing_offset_z
         offset_z = round(z_apex - self.z_expected, 4)
